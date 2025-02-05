13ba045433ac5df0aa8d5699d4ce381332dad291
luck-tech <luck@imai.am>
2025-02-05 14:40:56 +0900

    resize image

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,11 +8,14 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Workflow\WorkflowInterface;
 use Twig\Environment;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
     public function __construct(
@@ -21,7 +24,7 @@ class AdminController extends AbstractController
         private MessageBusInterface $bus
     ) {}
 
-    #[Route('/admin/comment/review/{id}', name: 'review_comment')]
+    #[Route('/comment/review/{id}', name: 'review_comment')]
     public function reviewComment(
         Request $request,
         Comment $comment,
@@ -52,5 +55,19 @@ class AdminController extends AbstractController
                 'comment' => $comment,
             ])
         );
+    }
+
+    #[Route('/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    public function purgeHttpCache(
+        KernelInterface $kernel,
+        Request $request,
+        string $uri,
+        StoreInterface $store
+    ): Response {
+        if ('prod' === $kernel->getEnvironment()) {
+            return new Response('KO', 400);
+        }
+        $store->purge($request->getSchemeAndHttpHost() . '/' . $uri);
+        return new Response('Done');
     }
 }
